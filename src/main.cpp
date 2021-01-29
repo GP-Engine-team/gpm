6d625c516599807b00b1d8cd5c7db7937c020726
s.amara <s.amara@student.isartdigital.com>
2021-01-29 17:26:51 +0100

    Added instanciation tests to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 #include "Vector2.hpp"
 #include "Vector3.hpp"
 #include "Vector4.hpp"
-
+#include "Quaternion.hpp"
 int main()
 {
-    GPM::Mat4 mat;
-    (void)mat;
+    GPM::Mat3 ma3;
+    GPM::Mat4 mat4;
+    GPM::Vec2 vec2;
+    GPM::Vec3 vec3;
+    GPM::Vec4 vec4;
+    GPM::Quat quat;
     
     return 0;
 }
